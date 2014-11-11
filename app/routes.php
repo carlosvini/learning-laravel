17d04f29a62480d1fcacc3aeebd3698bf4265973
jp7carlos <carlos.vini@gmail.com>
2014-11-11 18:27:37 -0200

    Lesson 4

--- app/routes.php
+++ app/routes.php
@@ -1,21 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the Closure to execute when that URI is requested.
-|
-*/
 
-Route::get('/', 'PagesController@home');
-Route::get('/about', 'PagesController@about');
+Route::get('/', function() {
 
-Route::get('/users', function() {
-  $users = User::find(1); // select * from users
+  $users = DB::table('users')->where('username', '!=', 'carlos')->get();
 
-  return $user->email;
+ return $users;
+
+
 });

c01f9d7ac95eca77ddc21208dbbb73b0a4e382ff
jp7carlos <carlos.vini@gmail.com>
2014-11-11 18:37:39 -0200

    Lesson 5

--- app/routes.php
+++ app/routes.php
@@ -3,9 +3,35 @@
 
 Route::get('/', function() {
 
-  $users = DB::table('users')->where('username', '!=', 'carlos')->get();
+  // $user = new User;
 
- return $users;
+  // $user->username = 'NewUser';
+  // $user->password = Hash::make('yahoo');
 
+  // $user->save(); 
+
+  // return $user;
+
+  // User::create([
+  //   'username' => 'NewUser',
+  //   'password' => Hash::make('yahoo')
+  // ]);
+
+
+  // $user = User::find(2);
+
+  // $user->username = 'UpdatedName';
+
+  // $user->save();
+
+
+  // $user = User::find(2);
+  // $user->delete();
+
+
+
+  //return User::all();
+
+  return User::orderBy('username', 'asc')->take(2)->get();
 
 });
